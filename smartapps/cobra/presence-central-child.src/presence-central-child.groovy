94091c96ad15a7f99c724c7d1800e036512e3a65
Andrew Parker <ajp@parker-it.co.uk>
2017-10-30 15:32:20 +0000

    30/10/2017

--- smartapps/cobra/presence-central-child.src/presence-central-child.groovy
+++ smartapps/cobra/presence-central-child.src/presence-central-child.groovy
@@ -137,8 +137,7 @@ def mainPage() {
     }             
       section() {
         	basicInputs()
-          	
-        	}
+          	  	}
         
   }
 }
--- smartapps/cobra/presence-central.src/presence-central.groovy
+++ smartapps/cobra/presence-central.src/presence-central.groovy
@@ -1,4 +1,4 @@
-﻿/**
+/**
  *  ****************  Presence Central  ****************
  *
  *  Design Usage:
