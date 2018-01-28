3a489b3b8fe1ee2eb11ce61b0b4d0b7f0febd659
CobraVmax <andrew.parker@securendpoint.com>
2018-01-28 23:25:39 +0000

    Updated 28/01/2018

--- smartapps/cobra/temperature-controlled-switch.src/temperature-controlled-switch.groovy
+++ smartapps/cobra/temperature-controlled-switch.src/temperature-controlled-switch.groovy
@@ -1,4 +1,4 @@
-/**
+﻿/**
  *  ****************  Temperature Controlled Switch  ****************
  *
  *	Credits: 
@@ -37,11 +37,13 @@
  *
  *-------------------------------------------------------------------------------------------------------------------
  *
- *  Created: 18/09/2017
- *  Last Update:18/11/2017
+ *  Created: 28/01/2018
+ *  Last Update:
  *
  *  Changes:
  *
+ *  V2.4.0 - Added variable external temp setting option (Using virtual controller DTH)
+ *  V2.3.0 - Added presence sensor restriction
  *  V2.2.1 - Debug installation issue where app would not install if a time was not set
  *  V2.2.0 - Added ability to use for cooling as well as heating
  *  V2.1.1 - Debug
@@ -60,10 +62,9 @@ definition(
     name: "Temperature Controlled Switch",
     namespace: "Cobra",
     author: "Andrew Parker",
-    description: "This SmartApp was designed to control a heater or cooler - turning on/off with  varying temperatures. \r\nIt has an optional 'override' switch and configurable restrictions on when it can or cannot run",
+    description: "This SmartApp was designed to control a heater - turning on/off with  varying temperatures. \r\nIt has an optional 'override' switch and configurable restrictions on when it can run \r\nYou can also use an optional external 'Temperature Controller'",
     category: "",
     
- //   parent: "Cobra:Group Central",
     
     iconUrl: "https://raw.githubusercontent.com/cobravmax/SmartThings/master/icons/temp.png",
 	iconX2Url: "https://raw.githubusercontent.com/cobravmax/SmartThings/master/icons/temp.png",
@@ -101,18 +102,17 @@ def initialize() {
     logCheck()
     state.enable = "on"
     log.info "state.enable = $state.enable"
-    
+    getpresenceOk()
     
 // Subscriptions ********************************************************************
-    subscribe(switch1, "switch", switchEnableNow) // Default - Enable/Disable switch
-// **********************************************************************************
-// Others...
 	subscribe(temperatureSensor1, "temperature", temperatureHandler)
+    if(switch1){ subscribe(switch1, "switch", switchEnableNow) } // Default - Enable/Disable switch
     if(ending){	schedule(ending, offNow) }
     if(contact1){ subscribe(contact1, "contact", contactHandler)}
+    if(restrictPresenceSensor) {subscribe (restrictPresenceSensor, "presence", presenceHandler)}
+    if(temperatureControl1) {subscribe (temperatureControl1, "temperature", customTempHandler)}
+   
 }
-
-
 
 
 // main page *************************************************************************
@@ -125,7 +125,7 @@ def introPage() {
         paragraph image: "https://raw.githubusercontent.com/cobravmax/SmartThings/master/icons/temp.png",
                   title: "Temperature Controlled Switch",
                   required: false,
-                 "This SmartApp was designed to control a heater or cooler - turning on/off with  varying temperatures. \r\nIt has an optional 'override' switch, contact sensor and configurable restrictions on when it can run"
+                 "This SmartApp was designed to control a heater or cooler - turning on/off with  varying temperatures. \r\nIt has optional, configurable restrictions on when it can run. \r\nIt also has an optional 'Temperature Control Switch' to control temperature setting."
                   }
                   
         section() {   
@@ -137,6 +137,9 @@ def introPage() {
 		section("Basic App Settings") {
         input "appmode", "bool", title: " Select mode of operation\r\n Off = Heating - On = Cooling", required: true, submitOnChange: true, defaultValue: false    
 		input "switch1", "capability.switch", title: "Select switch to enable/disable app (Optional)", required: false, multiple: false 
+        }
+        section("Only allow action if this sensor is 'Present'") {
+        input "restrictPresenceSensor", "capability.presenceSensor", title: "Select presence sensor (Optional)", required: false, multiple: false
     }  
 }
 
@@ -164,12 +167,25 @@ def settingsPage(){
 // Input Page  *********************************************************************
 def inputPage(){
 	 dynamicPage(name: "inputPage") {
-     section("Select Temperature Sensor") {
-		input "temperatureSensor1", "capability.temperatureMeasurement" , required: true
+     section() {
+		input "temperatureSensor1", "capability.temperatureMeasurement" , title: "Select Temperature Sensor", required: true
 	}
+    
+    
+    
+    
 	section("Desired Temperature") {
+    
+    input "tempMode", "bool", title: " Select mode of operation\r\n Off = Fixed Temperature - On = Variable Temperature", required: true, submitOnChange: true, defaultValue: false  
+    if(tempMode == true){
+    input "temperatureControl1", "capability.temperatureMeasurement" , title: "Select Controller", required: true
+    
+    }
+   
+    else if(tempMode == false){
 		input "temperature1", "number", title: "Temperature?", required: true
 	}
+   }
    	section("Control this Switch/Heater/Cooler...") {
 		input "switch2", "capability.switch", required: true, multiple: true
 	}
@@ -200,6 +216,29 @@ def namePage() {
 
 // Handlers & Actions *****************************
 
+def customTempHandler(evt){
+state.reqTemp = evt.doubleValue
+LOGDEBUG("Required Temp set to: $state.reqTemp degrees by virtual controller: $temperatureControl1 ")
+runTemp()
+}
+
+
+
+def presenceHandler(evt){
+state.presence1Now = evt.value
+if (state.presence1Now == 'not present'){
+	LOGDEBUG("Presence is $state.presence1Now - Switching off now...")
+switch2.off()
+	LOGDEBUG("$switch2 is OFF - Heating/Cooling Disabled")
+	}
+ else if (state.presence1Now == 'present'){
+LOGDEBUG("Presence is $state.presence1Now - Heating/Cooling Allowed")
+switch2.on()
+	}
+}
+
+
+
 def contactHandler(evt){
 	state.contact1Now = evt.value
 
@@ -210,8 +249,7 @@ switch2.off()
 	}
  else{
 LOGDEBUG("Contact is $state.contact1Now - Heating/Cooling Allowed")
-
- }
+	}
 }
 
 def offNow(){
@@ -219,47 +257,71 @@ LOGDEBUG("Time expired.. Switching off now...")
 switch2.off()
 }
 
+
+
 def temperatureHandler(evt) {
+	state.newTemp = evt.doubleValue
+LOGDEBUG("requested temperature = $state.newTemp degrees")
+	runTemp()
+}
+
+def runTemp(){
+	def currTemp = state.newTemp
+LOGDEBUG("Reported temperature is now: $currTemp degrees.")
+
 	if(allOk){    
 LOGDEBUG("All ok so can continue...")
 
 	if(appmode == false){
 	LOGDEBUG("Configured for heating mode")
-	def myTemp = temperature1
-	// Is reported temp below or above setting?	
-	if (evt.doubleValue < myTemp) {
-		
-		LOGDEBUG( "Reported temperature is below $myTemp so activating $switch2")
+    
+	if(tempMode == true){ 
+     state.confTemp = state.reqTemp
+     LOGDEBUG("Configured to use variable temp controller - state.confTemp = $state.confTemp")
+	}  
+    else if(tempMode == false){
+    state.confTemp = temperature1
+    LOGDEBUG("Configured to use fixed temp - state.confTemp = $state.confTemp")
+    }
+    
+	// Is reported temp below required setting?	
+    if (currTemp < state.confTemp) {
+	LOGDEBUG( "Reported temperature is below $state.confTemp so activating $switch2")
 			switch2.on()
-		}
-        else if (evt.doubleValue >= myTemp) {
-       LOGDEBUG( "Reported temperature is equal to, or above, $myTemp so deactivating $switch2")
+	}
+    else if (currTemp >= state.confTemp) {
+    LOGDEBUG( "Reported temperature is equal to, or above, $state.confTemp so deactivating $switch2")
 			switch2.off()
 	}
 }
+
 else if(appmode == true){
 LOGDEBUG("Configured for cooling mode")
-	def myTemp = temperature1
-	// Is reported temp below or above setting?	
-	if (evt.doubleValue > myTemp) {
-		
-		LOGDEBUG( "Reported temperature is above $myTemp so activating $switch2")
+
+    if(tempMode == true){   
+	state.confTemp = state.reqTemp
+     LOGDEBUG("Configured to use variable temp controller - state.confTemp = $state.confTemp")
+    }
+    else if(tempMode == false){
+    state.confTemp = temperature1
+    LOGDEBUG("Configured to use fixed temp - state.confTemp = $state.confTemp")
+    }
+
+	// Is reported temp above setting?	
+	if (currTemp > state.confTemp) {	
+	LOGDEBUG( "Reported temperature is above $myTemp so activating $switch2")
 			switch2.on()
-		}
-        else if (evt.doubleValue <= myTemp) {
-       LOGDEBUG( "Reported temperature is equal to, or below, $myTemp so deactivating $switch2")
+	}
+    else if (currTemp <= state.confTemp) {
+    LOGDEBUG( "Reported temperature is equal to, or below, $myTemp so deactivating $switch2")
 			switch2.off()
 	}
-}
-
-
-
+  }
 }
 	else if(!allOk){
 LOGDEBUG(" Not ok - one or more conditions are not met")
-LOGDEBUG("modeOk = $modeOk - daysOk = $daysOk - timeOk = $timeOk - enableOk = $enableOk")
-
-}
+LOGDEBUG("modeOk = $modeOk - daysOk = $daysOk - timeOk = $timeOk - enableOk = $enableOk - presenceOK = $presenceOk")
+	}
 }
 
 
@@ -267,18 +329,32 @@ LOGDEBUG("modeOk = $modeOk - daysOk = $daysOk - timeOk = $timeOk - enableOk = $e
 
 // disable/enable switch
 def switchEnableNow(evt){
-state.enable = evt.value
- 
-  LOGDEBUG( "Enable/Disable switch $switch1 is $state.currS1")
-   if (state.enable == "off" ) {
-   switch2.off()
+	state.enable = evt.value
+	LOGDEBUG( "Enable/Disable switch $switch1 is $state.currS1")
+	if (state.enable == "off" ) {
+		switch2.off()
    }
 }
 
 
 private getAllOk() {
-	modeOk && daysOk && timeOk && enableOk && contactOk
+	modeOk && daysOk && timeOk && enableOk && contactOk && presenceOk
 }
+
+
+private getpresenceOk() {
+	def result = true
+		if (state.presence1Now == 'not present' ) {
+	result = false
+	}
+    else  {
+	result = true
+    }
+    LOGDEBUG("presenceOk = $result")
+	result
+}
+
+
 
 
 private getcontactOk() {
@@ -291,13 +367,10 @@ private getcontactOk() {
     }
     LOGDEBUG("contactOk = $result")
 	result
-    
-
 }
 
 
-private getmodeOk() {
-	
+private getModeOk() {
 	def result = !modes || modes.contains(location.mode)
 	LOGDEBUG("modeOk = $result")
 	result
@@ -328,10 +401,6 @@ private getTimeOk() {
 		def stop = timeToday(ending).time
 		result = start < stop ? currTime >= start && currTime <= stop : currTime <= stop || currTime >= start
 	}
-    else if (!starting && !ending) {
-    LOGDEBUG("No time restrictions configured... Continue")
-//    def result = true
-    }
 	LOGDEBUG("timeOk = $result")
 	result
 }
@@ -349,14 +418,10 @@ private getTimeIntervalLabel(){
 
 
 private getenableOk(){
-def result = true
-if(state.enable == 'on'){
-result = true }
-
-else if(state.enable == 'off'){
-result = false }
-
-LOGDEBUG("enableOk = $result")
+	def result = true
+	if(state.enable == 'on'){result = true }
+	else if(state.enable == 'off'){result = false }
+	LOGDEBUG("enableOk = $result")
 	result
 }
 
@@ -368,15 +433,15 @@ private hideOptionsSection() {
 
 // define debug action
 def logCheck(){
-state.checkLog = debugMode
-if(state.checkLog == true){
-log.info "All Logging Enabled"
-}
-else if(state.checkLog == false){
-log.info "Further Logging Disabled"
+	state.checkLog = debugMode
+	if(state.checkLog == true){
+	log.info "All Logging Enabled"
+	}
+	else if(state.checkLog == false){
+	log.info "Further Logging Disabled"
+	}
 }
 
-}
 def LOGDEBUG(txt){
     try {
     	if (settings.debugMode) { log.debug("${app.label.replace(" ",".").toUpperCase()}  (AppVersion: ${state.appversion})  ${txt}") }
@@ -390,5 +455,5 @@ def LOGDEBUG(txt){
 
 // App Version   *********************************************************************************
 def setAppVersion(){
-    state.appversion = "2.2.1"
+    state.appversion = "2.4.0"
 }
